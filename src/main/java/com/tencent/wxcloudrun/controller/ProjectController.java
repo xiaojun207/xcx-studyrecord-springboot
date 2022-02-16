943ef42cace0e1dcd3e1a38b9dab326518fe2476
name <email@example.com>
2022-02-16 17:06:24 +0800

    add projectList

--- src/main/java/com/tencent/wxcloudrun/controller/ProjectController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/ProjectController.java
@@ -1,12 +1,11 @@
 package com.tencent.wxcloudrun.controller;
 
 import com.tencent.wxcloudrun.config.ApiResponse;
-import com.tencent.wxcloudrun.model.UserTest;
-import com.tencent.wxcloudrun.model.WxAccount;
 import com.tencent.wxcloudrun.service.ProjectService;
-import com.tencent.wxcloudrun.service.UserTestService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
